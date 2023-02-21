cbfc253fb872b3b45008e2b6732f34461ba02305
Jhorlan-Ortega <124930448+Jhorlan-Ortega@users.noreply.github.com>
2023-02-20 23:51:15 -0500

    Intento_8

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
 class Asiento:
 
-    def _init_(self,color,precio,registro):
+    def _init_(self, color, precio, registro):
         self.color=color
         self.precio=precio
         self.regristro=registro
+        
     def cambiarColor(self,color):
-        if color=="rojo"or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco" :
+        if color=="rojo":
+            self.color=color
+        elif color=="verde":
+            self.color=color
+        elif color=="amarillo":
+            self.color=color 
+        elif color=="negro":
+            self.color=color
+        elif color=="blanco":
             self.color=color
 
-
 class Auto:
-
-    def _init_(self,modelo,precio,asientos,marca,motor,registro):
+    cantidadcreados=0
+    def _init_(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo=modelo
         self.precio=precio
-        self.asientos=list(asientos)
+        self.asientos=asientos
         self.marca=marca
         self.motor=motor
         self.registro=registro
     def cantidadAsientos(self):
-        numAsientos=0
-        if self.asientos !=None:
-            for i in self.asientos:
-                if i != None:
-                    numAsientos+=1
-        return numAsientos
+        numeroAsientos=0
+        
+        for asiento in self.asientos:
+            if (type(asiento))==Asiento:
+                    numeroAsientos=numeroAsientos+1
+        return numeroAsientos
 
     def verificarIntegridad(self):
         mensaje1="Auto original"
         mensaje2="Las piezas no son originales"
-        if self.registro==self.asientos[0].registro:
-            if self.motor.registro==self.registro:
-                return mensaje1
-            else:
-                return mensaje2
+        if self.registro==self.motor.registro:
+            for asiento in self.asientos:
+                if(type(asiento)==Asiento):
+                    if asiento.registro!=self.registro:
+                        return mensaje2
+            return mensaje2
         return mensaje2
 
 class Motor:
@@ -42,10 +51,12 @@ class Motor:
         self.numeroCilindros=numeroCilindros
         self.tipo=tipo
         self.registro=registro
-    def cambiarRegistro(self,registro):
+
+    def cambiarRegistro(self, registro):
         self.registro=registro
+
     def asignarTipo(self,tipo):
-        if (tipo=="electrico"or tipo=="gasolina"):
+        if (tipo=="electrico" or tipo=="gasolina"):
             self.tipo=tipo
 
 
