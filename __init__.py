464d7e041519b97dd1042dad2986f7bf230480a8
Jhorlan-Ortega <124930448+Jhorlan-Ortega@users.noreply.github.com>
2023-02-20 21:52:35 -0500

    Intento_1

---
+++ __init__.py
@@ -0,0 +1,55 @@
+class Asiento:
+    pass
+    def _init_(self,color,precio,registro):
+        self.color=color
+        self.precio=precio
+        self.regristro=registro
+    def cambiarColor(self,color):
+        if color=="rojo"or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco" :
+            self.color=color
+
+
+class Auto:
+    pass
+    def _init_(self,modelo,precio,asientos,marca,motor,registro):
+        self.modelo=modelo
+        self.precio=precio
+        self.asientos=list(asientos)
+        self.marca=marca
+        self.motor=motor
+        self.registro=registro
+    def cantidadAsientos(self):
+        numAsientos=0
+        for asiento in self.asientos:
+            if (type(asiento)==Asiento):
+                ++numAsientos
+        return numAsientos
+
+    def verificarIntegridad(self):
+        mensaje1="Auto original"
+        mensaje2="Las piezas no son originales"
+        if (self.registro==self.asientos[0].registro):
+            if self.motor.registro==self.registro:
+                return mensaje1
+            else:
+                return mensaje2
+        return mensaje1
+class Motor:
+    pass
+    def _init_(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros=numeroCilindros
+        self.tipo=tipo
+        self.registro=registro
+    def cambiarRegistro(self,registro):
+        self.registro=registro
+    def asignarTipo(self,tipo):
+        if (tipo=="electrico"or tipo=="gasolina"):
+            self.tipo=tipo
+
+
+
+
+
+
+
+
